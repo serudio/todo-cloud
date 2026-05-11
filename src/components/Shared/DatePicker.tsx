af8e100ce8e36e023ecbc6159c423b5fa2e0aae3
serhii <cestserg@gmail.com>
2026-05-11 09:00:07 +0300

    date calendar start on monday

--- src/components/Shared/DatePicker.tsx
+++ src/components/Shared/DatePicker.tsx
@@ -5,8 +5,14 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { useState } from "react";
 import dayjs from "dayjs";
+import "dayjs/locale/uk";
 import type { Dayjs } from "dayjs";
 import { formatDateKey } from "../../utils/todos";
+import updateLocale from "dayjs/plugin/updateLocale";
+
+// Extend dayjs with the updateLocale plugin
+dayjs.extend(updateLocale);
+dayjs.updateLocale("en", { weekStart: 1 }); // 0 = Sunday, 1 = Monday
 
 type Props = {
   value: number | null;
@@ -60,8 +66,13 @@ export const DatePicker: React.FC<Props> = ({ value, onChange, onOpen }) => {
         anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
         transformOrigin={{ vertical: "top", horizontal: "center" }}
       >
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar value={value ? dayjs(value) : null} onChange={handleDueDateChange} />
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="enGB">
+          <DateCalendar
+            value={value ? dayjs(value) : null}
+            onChange={handleDueDateChange}
+            displayWeekNumber
+            showDaysOutsideCurrentMonth
+          />
         </LocalizationProvider>
         <Box sx={{ display: "flex", justifyContent: "flex-end", px: 2, pb: 1.5 }}>
           <Button disabled={!value} onClick={clearDueDate} size="small" variant="text">
