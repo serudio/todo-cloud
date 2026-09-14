4c31ee16a21375356507d3727fe76562a3f5baba
serhii <cestserg@gmail.com>
2026-09-14 15:36:08 +0300

    done maxheight

--- src/components/DoneCard/DoneCard.tsx
+++ src/components/DoneCard/DoneCard.tsx
@@ -18,7 +18,7 @@ export const DoneCard: React.FC<DoneListProps> = ({ todos, updateTodo, tags, onD
     <SectionCard title="Done" sx={{ overflow: "auto" }}>
       {doneTodos.length === 0 && <p>Done items will show up here.</p>}
 
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 1, overflow: "auto" }}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 1, overflow: "auto", maxHeight: 600 }}>
         {doneTodos.map((todo) => {
           return <DoneItem key={todo.id} item={todo} tags={tags} updateTodo={updateTodo} onDeleteTodo={onDeleteTodo} />;
         })}
